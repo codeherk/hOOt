48fbc5a0c1aa3f61f6eca966f2545d6fb39e7a3f
Erik Rosales <erikrosales@f4-0f-24-34-e5-15.wireless.temple.edu>
2019-03-20 13:08:58 -0400

    First commit

--- alexa/lambda/custom/index.js
+++ alexa/lambda/custom/index.js
@@ -253,6 +253,7 @@ const LaunchRequestHandler = {
     return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
   },
   handle(handlerInput) {
+<<<<<<< HEAD
     if (handlerInput.requestEnvelope.context.System.user.accessToken === undefined) {
       return handlerInput.responseBuilder
         .speak("to start using this skill, please use the companion app to authenticate")
@@ -299,6 +300,16 @@ const LaunchRequestHandler = {
       });
       }
     },
+=======
+    const speechText = 'Welcome to Hoot for Canvas, how may I help you?';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard('Hoot for Canvas', speechText)
+      .getResponse();
+  },
+>>>>>>> First commit
 };
 
 /**
